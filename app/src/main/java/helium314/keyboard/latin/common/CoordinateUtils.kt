8b9ed8da1b5c185ecb6033b06c28050fe2ca5851
Schuyler Lewis <elvilewis40@gmail.com>
2026-09-25 19:57:00 +0530

    [2/10] Deploy remix-remix_-vianbr-media via Vian Github Help

--- app/src/main/java/helium314/keyboard/latin/common/CoordinateUtils.kt
+++
@@ -1,8 +0,0 @@
-package helium314.keyboard.latin.common
-
-import com.example.foundation.utils.CoordinateUtils as FoundationCoordinateUtils
-
-/**
- * Clean bridge alias mapping legacy coordinate imports to modern Batch 1 foundation package.
- */
-typealias CoordinateUtils = FoundationCoordinateUtils
--- app/src/main/java/helium314/keyboard/latin/settings/SettingsValuesForSuggestion.kt
+++
@@ -1,6 +0,0 @@
-package helium314.keyboard.latin.settings
-
-class SettingsValuesForSuggestion(
-    val mAutoCorrectEnabled: Boolean = false,
-    val mPredictNextWord: Boolean = true
-)
